bcc0ed29c4724c5517775acc66a160cfa8d68f8f
Henrique Goetz <henrique.goetz@gmail.com>
2026-02-05 11:13:58 -0300

    :sparkles: Front-end da tela de cadastro de empresa

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,92 @@
-import './bootstrap';
+import "./bootstrap";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("cadastro-empresa");
+    if (!form) return;
+
+    const nomeInput = form.querySelector('input[name="nome"]');
+    const cnpjInput = form.querySelector('input[name="cnpj"]');
+    const icmsPagoInput = form.querySelector('input[name="icms_pago"]');
+    const creditosPossiveisInput = form.querySelector(
+        'input[name="creditos_possiveis"]',
+    );
+
+    const loading = form.querySelector("#loading");
+
+    form.addEventListener("submit", async (e) => {
+        e.preventDefault();
+        loading.style.display = "flex";
+        try {
+            const response = await axios.post(
+                "/api/empresa",
+                JSON.stringify({
+                    nome: nomeInput.value,
+                    cnpj: cnpjInput.value,
+                    icms_pago: icmsPagoInput.value.replace(/\D/g, ""),
+                    creditos_possiveis: creditosPossiveisInput.value.replace(
+                        /\D/g,
+                        "",
+                    ),
+                }),
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Accept: "application/json",
+                    },
+                },
+            );
+
+            if (response.ok) {
+                alert("Enviado com sucesso!");
+            }
+        } catch (error) {
+            alert("Ocorreu um erro ao enviar o formulário.");
+        }
+
+        loading.style.display = "none";
+    });
+
+    function maskCNPJ(value) {
+        return value
+            .replace(/\D/g, "")
+            .replace(/^(\d{2})(\d)/, "$1.$2")
+            .replace(/^(\d{2})\.(\d{3})(\d)/, "$1.$2.$3")
+            .replace(/\.(\d{3})(\d)/, ".$1/$2")
+            .replace(/(\d{4})(\d)/, "$1-$2")
+            .slice(0, 18);
+    }
+
+    if (cnpjInput) {
+        cnpjInput.addEventListener("input", (e) => {
+            e.target.value = maskCNPJ(e.target.value);
+        });
+    }
+
+    function maskMoney(value) {
+        const numeric = value.replace(/\D/g, "");
+        const number = (Number(numeric) / 100).toFixed(2);
+
+        return number.replace(".", ",").replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    }
+
+    function applyMoneyMask(input) {
+        if (!input) return;
+
+        input.addEventListener("input", (e) => {
+            let valor = input.value.replace(/\D/g, "");
+            if (!valor) {
+                input.value = "R$ 0,00";
+                return;
+            }
+
+            valor = (parseInt(valor, 10) / 100).toFixed(2);
+            valor = valor.replace(".", ",");
+            valor = valor.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+            input.value = `R$ ${valor}`;
+        });
+    }
+
+    applyMoneyMask(icmsPagoInput);
+    applyMoneyMask(creditosPossiveisInput);
+});
---
+++ resources/views/empresa.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>{{ config('app.name', 'Laravel') }}</title>
+  <meta name="description" content="Teste prático de programação utilizando Laravel">
+  <!-- Fonts -->
+  <link rel="preconnect" href="https://fonts.bunny.net">
+  <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
+  <!-- Styles / Scripts -->
+  @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+  @endif
+</head>
+
+<body class="relative min-h-[100dvh] bg-[#222221] text-white py-[60px]">
+  <img src="{{ asset('/grupo-studio.png') }}" alt="Grupo Studio" class="w-full max-w-[320px] h-auto mx-auto mt-10" />
+  <form id="cadastro-empresa" class="mt-[40px] w-full max-w-[480px] mx-auto">
+    <h1 class="mx-auto mb-10 text-center text-[24px]">Calcule seu crédito tributário</h1>
+    <label class="mb-[10px] flex flex-col gap-[6px]">
+      <span class="">Nome da empresa</span>
+      <input type="text" required name="nome" placeholder="Digite o nome da empresa"
+        class="bg-[#eee] rounded-[10px] p-3 w-full text-gray-700" />
+    </label>
+    <label class="mb-[10px] flex flex-col gap-[6px]">
+      <span class="">CNPJ</span>
+      <input type="text" required name="cnpj" placeholder="Digite o CNPJ"
+        class="bg-[#eee] rounded-[10px] p-3 w-full text-gray-700" />
+    </label>
+    <label class="mb-[10px] flex flex-col gap-[6px]">
+      <span class="">ICMS Pago</span>
+      <input type="text" required name="icms_pago" placeholder="Informe o valor do ICMS pago"
+        class="bg-[#eee] rounded-[10px] p-3 w-full text-gray-700" />
+    </label>
+    <label class="mb-[10px] flex flex-col gap-[6px]">
+      <span class="">Créditos Possíveis</span>
+      <input type="text" required name="creditos_possiveis" placeholder="Informe o valor dos créditos possíveis"
+        class="bg-[#eee] rounded-[10px] p-3 w-full text-gray-700" />
+    </label>
+    <button type="submit"
+      class="cursor-pointer mt-[10px] bg-[#8b6f4c] hover:bg-[#777] text-white font-bold py-3 px-6 rounded-[10px] w-full">Calcular
+    </button>
+    <div id="loading" class="hidden absolute top-0 bottom-0 left-0 right-0 bg-[rgba(200,200,200,0.5)] items-center justify-center">
+      <img src="{{ asset('/logo.png') }}" alt="GS"
+        class="w-full max-w-[70px] h-auto mx-auto mt-10 animate-bounce" />
+    </div>
+  </form>
+</body>
+
+</html>
